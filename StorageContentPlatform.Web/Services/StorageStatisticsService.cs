0a3519059f5e10b88451ec689a68789248a4c43a
Massimo Bonanni <massimo.bonanni@tiscali.it>
2025-02-11 14:36:02 +0100

    Add support for DefaultAzureCredential in storage service
    Enhanced StorageStatisticsService to support DefaultAzureCredential when StorageConnectionString is not provided. Added StorageAccountName property and refactored LoadConfig method. Introduced CreateTableClient method for flexible client creation. Improved code organization with regions for better readability.

--- StorageContentPlatform.Web/Services/StorageStatisticsService.cs
+++ StorageContentPlatform.Web/Services/StorageStatisticsService.cs
@@ -1,4 +1,5 @@
 ﻿using Azure.Data.Tables;
+using Azure.Identity;
 using Azure.Storage.Blobs.Models;
 using StorageContentPlatform.Web.Entities;
 using StorageContentPlatform.Web.Interfaces;
@@ -10,7 +11,7 @@ namespace StorageContentPlatform.Web.Services
         private class Configuration
         {
             public string StorageConnectionString { get; set; }
-
+            public string StorageAccountName { get; set; }
             public string StatisticTableName { get; set; }
         }
 
@@ -23,25 +24,21 @@ namespace StorageContentPlatform.Web.Services
             this.configurationValues = new Configuration();
         }
 
-        private void LoadConfig()
-        {
-            this.configurationValues.StorageConnectionString = this.configuration.GetValue<string>("StorageConnectionString");
-            this.configurationValues.StatisticTableName = this.configuration.GetValue<string>("StatisticTableName");
-        }
-
+        #region [ Public Method - IStatisticsService implementation ]
         public async Task<IEnumerable<StatisticData>> GetStatisticsAsync(DateTime to, DateTime from)
         {
             var result = new List<StatisticData>();
             LoadConfig();
 
-            var filterString=$"InventoryStartTime ge datetime'{from.ToString("yyyy-MM-ddT00:00:00Z")}' and InventoryStartTime le datetime'{to.ToString("yyyy-MM-ddT23:59:59Z")}'";
-
-            var tableServiceClient = new TableServiceClient(this.configurationValues.StorageConnectionString);
+            var filterString = $"InventoryStartTime ge datetime'{from.ToString("yyyy-MM-ddT00:00:00Z")}' and InventoryStartTime le datetime'{to.ToString("yyyy-MM-ddT23:59:59Z")}'";
+            
+            TableServiceClient tableServiceClient = CreateTebleClient
+                ();
             var tableClient = tableServiceClient.GetTableClient(this.configurationValues.StatisticTableName);
             var queryResult = tableClient
                 .QueryAsync<TableEntity>(filterString)
                 .AsPages(default, 100);
-            
+
             await foreach (var tablePage in queryResult)
             {
                 foreach (var tableItem in tablePage.Values)
@@ -49,8 +46,37 @@ namespace StorageContentPlatform.Web.Services
                     result.Add(tableItem.ToStatisticData());
                 }
             }
-            
+
             return result.OrderByDescending(s => s.InventoryStartTime);
         }
+
+        #endregion [ Public Method - IStatisticsService implementation ]
+
+        #region [ Private Methods ]
+        private void LoadConfig()
+        {
+            this.configurationValues.StorageConnectionString = this.configuration.GetValue<string>("StorageConnectionString");
+            this.configurationValues.StorageAccountName = this.configuration.GetValue<string>("StorageAccountName");
+            this.configurationValues.StatisticTableName = this.configuration.GetValue<string>("StatisticTableName");
+        }
+
+        private TableServiceClient CreateTebleClient()
+        {
+            TableServiceClient tableClient = null;
+            if (!string.IsNullOrWhiteSpace(this.configurationValues.StorageConnectionString))
+            {
+                tableClient = new TableServiceClient(
+                    this.configurationValues.StorageConnectionString);
+            }
+            else
+            {
+                tableClient= new TableServiceClient(
+                    new Uri($"https://{this.configurationValues.StorageAccountName}.table.core.windows.net"),
+                    new DefaultAzureCredential());
+            }
+            return tableClient;
+        }
+
+        #endregion [ Private Methods ]
     }
 }
